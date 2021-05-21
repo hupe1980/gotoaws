ef939a6105059d7ffc5e04415c1016ff4281e3a6
hupe1980 <frankhuebner1980@gmail.com>
2021-05-21 21:02:07 +0200

    Add plugin search

--- internal/config.go
+++ internal/config.go
@@ -2,7 +2,9 @@ package internal
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -34,9 +36,17 @@ func NewConfig(profile string, region string) (*Config, error) {
 		awsCfg.Region = region
 	}
 
+	pluginPath, err := exec.LookPath("xsession-manager-plugin")
+	if err != nil {
+		return nil, fmt.Errorf(`session-manager-plugin not found
+		
+https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
+		`)
+	}
+
 	return &Config{
 		profile: profile,
-		plugin:  "session-manager-plugin",
+		plugin:  pluginPath,
 		awsCfg:  awsCfg,
 	}, nil
 }
