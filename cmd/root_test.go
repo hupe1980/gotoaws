e90a14a2ae0b7a02cafe87ffe5133df601f0273b
hupe1980 <frankhuebner1980@gmail.com>
2021-05-24 00:43:46 +0200

    Add tests

---
+++ cmd/root_test.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRootCmdHelp(t *testing.T) {
+	var b bytes.Buffer
+	cmd := newRootCmd("")
+	cmd.SetOut(&b)
+	cmd.SetArgs([]string{"-h"})
+	require.NoError(t, cmd.Execute())
+}
+func TestRootCmdVersion(t *testing.T) {
+	var b bytes.Buffer
+	cmd := newRootCmd("1.2.3")
+	cmd.SetOut(&b)
+	cmd.SetArgs([]string{"-v"})
+	require.NoError(t, cmd.Execute())
+	require.Equal(t, "awsconnect version 1.2.3\n", b.String())
+}
